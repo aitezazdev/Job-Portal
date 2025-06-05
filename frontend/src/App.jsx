558c541a7669f1b0be036f9e8909f959bc4000e4
aitezazdev <aitezazsikandar@gmail.com>
2025-06-05 14:41:17 +0500

    app.jsx setting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import JobsListing from './Pages/JobsListing'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <main>
-        <JobsListing />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs-listing" element={<JobsListing />} />
+        </Routes>
       </main>
     </div>
   )
